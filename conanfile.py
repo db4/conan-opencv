740514dce386695fbd761d662b0f44a36fff5792
Dmitry Bely <d.bely@recognize.ru>
2018-03-16 12:17:34 +0300

    Move cmake config parsing code to a separate cmake_config_tools package, update TBB

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
-import re
 import json
-from io import StringIO
 
 CONAN_REPO = "https://github.com/db4/conan-opencv"
 OPENCV_REPO = "https://github.com/opencv/opencv.git"
@@ -10,239 +8,7 @@ OPENCV_VERSION = "3.4.0"
 OPENCV_BRANCH = "tags/" + OPENCV_VERSION
 
 QT_PKG = None
-TBB_PKG = "TBB/4.4.4@memsharded/testing"
-
-CMAKELISTS_TXT = r"""
-cmake_minimum_required(VERSION 2.8.12)
-# --- begin CMakeExpandImportedTargets.cmake ---
-
-#.rst:
-# CMakeExpandImportedTargets
-# --------------------------
-# Dmitry Bely <dmitry.bely@gmail.com>:
-#   Import lib support, parse $<LINK_ONLY:(.*)> generator expressions, exclude duplicates
-#
-# ::
-#
-#  CMAKE_EXPAND_IMPORTED_TARGETS(<var> LIBRARIES lib1 lib2...libN
-#                                [CONFIGURATION <config>])
-#
-# CMAKE_EXPAND_IMPORTED_TARGETS() takes a list of libraries and replaces
-# all imported targets contained in this list with their actual file
-# paths of the referenced libraries on disk, including the libraries
-# from their link interfaces.  If a CONFIGURATION is given, it uses the
-# respective configuration of the imported targets if it exists.  If no
-# CONFIGURATION is given, it uses the first configuration from
-# ${CMAKE_CONFIGURATION_TYPES} if set, otherwise ${CMAKE_BUILD_TYPE}.
-# This macro is used by all Check*.cmake files which use try_compile()
-# or try_run() and support CMAKE_REQUIRED_LIBRARIES , so that these
-# checks support imported targets in CMAKE_REQUIRED_LIBRARIES:
-#
-# ::
-#
-#     cmake_expand_imported_targets(expandedLibs
-#       LIBRARIES ${CMAKE_REQUIRED_LIBRARIES}
-#       CONFIGURATION "${CMAKE_TRY_COMPILE_CONFIGURATION}" )
-
-
-#=============================================================================
-# Copyright 2012 Kitware, Inc.
-# Copyright 2009-2012 Alexander Neundorf <neundorf@kde.org>
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
-
-include(CMakeParseArguments)
-
-function(CMAKE_EXPAND_IMPORTED_TARGETS _RESULT )
-
-   set(options )
-   set(oneValueArgs CONFIGURATION )
-   set(multiValueArgs LIBRARIES )
-
-   cmake_parse_arguments(CEIT "${options}" "${oneValueArgs}" "${multiValueArgs}"  ${ARGN})
-
-   if(CEIT_UNPARSED_ARGUMENTS)
-      message(FATAL_ERROR "Unknown keywords given to CMAKE_EXPAND_IMPORTED_TARGETS(): \"${CEIT_UNPARSED_ARGUMENTS}\"")
-   endif()
-
-   if(NOT CEIT_CONFIGURATION)
-      if(CMAKE_CONFIGURATION_TYPES)
-         list(GET CMAKE_CONFIGURATION_TYPES 0 CEIT_CONFIGURATION)
-      else()
-         set(CEIT_CONFIGURATION ${CMAKE_BUILD_TYPE})
-      endif()
-      string(TOUPPER ${CEIT_CONFIGURATION} CEIT_CONFIGURATION)
-   endif()
-
-   # handle imported library targets
-
-   set(_CCSR_REQ_LIBS ${CEIT_LIBRARIES})
-
-   set(_CHECK_FOR_IMPORTED_TARGETS TRUE)
-   set(_CCSR_LOOP_COUNTER 0)
-   while(_CHECK_FOR_IMPORTED_TARGETS)
-      math(EXPR _CCSR_LOOP_COUNTER "${_CCSR_LOOP_COUNTER} + 1 ")
-      set(_CCSR_NEW_REQ_LIBS )
-      set(_CHECK_FOR_IMPORTED_TARGETS FALSE)
-      foreach(_CURRENT_LIB ${_CCSR_REQ_LIBS})
-         if(TARGET "${_CURRENT_LIB}")
-            get_target_property(_imported "${_CURRENT_LIB}" IMPORTED)
-         else()
-            set(_imported "")
-         endif()
-         if (_imported)
-#            message(STATUS "Detected imported target ${_CURRENT_LIB}")
-            # Ok, so this is an imported target.
-            # First we get the imported configurations.
-            # Then we get the location of the actual library on disk of the first configuration.
-            # then we'll get its link interface libraries property,
-            # iterate through it and replace all imported targets we find there
-            # with there actual location.
-
-            # guard against infinite loop: abort after 100 iterations ( 100 is arbitrary chosen)
-            if ("${_CCSR_LOOP_COUNTER}" LESS 100)
-               set(_CHECK_FOR_IMPORTED_TARGETS TRUE)
-#                else ()
-#                   message(STATUS "********* aborting loop, counter : ${_CCSR_LOOP_COUNTER}")
-            endif ()
-
-            get_target_property(_importedConfigs "${_CURRENT_LIB}" IMPORTED_CONFIGURATIONS)
-            if (_importedConfigs)
-               # if one of the imported configurations equals ${CMAKE_TRY_COMPILE_CONFIGURATION},
-               # use it, otherwise skip:
-               list(FIND _importedConfigs "${CEIT_CONFIGURATION}" _configIndexToUse)
-               if(NOT "${_configIndexToUse}" EQUAL -1)
-                  list(GET _importedConfigs ${_configIndexToUse} _importedConfigToUse)
-
-                  get_target_property(_importedLocation "${_CURRENT_LIB}" IMPORTED_IMPLIB_${_importedConfigToUse})
-                  if(NOT _importedLocation)
-                     get_target_property(_importedLocation "${_CURRENT_LIB}" IMPORTED_LOCATION_${_importedConfigToUse})
-                  endif()
-                  list(APPEND _CCSR_NEW_REQ_LIBS  "${_importedLocation}")
-#                  message(STATUS "Appending lib ${_CURRENT_LIB} as ${_importedLocation}")
-
-                  get_target_property(_linkInterfaceLibs "${_CURRENT_LIB}" IMPORTED_LINK_INTERFACE_LIBRARIES_${_importedConfigToUse} )
-                  if(NOT _linkInterfaceLibs)
-                     get_target_property(_linkInterfaceLibs "${_CURRENT_LIB}" INTERFACE_LINK_LIBRARIES)
-                  endif()
-                  if(_linkInterfaceLibs)
-                     foreach(_currentLinkInterfaceLib ${_linkInterfaceLibs})
-                        if("${_currentLinkInterfaceLib}" MATCHES "\\\$<LINK_ONLY:(.*)>")
-                           set(_currentLinkInterfaceLib ${CMAKE_MATCH_1})
-                        endif()
-#                        message(STATUS "Appending link interface lib ${_currentLinkInterfaceLib}")
-                        if(_currentLinkInterfaceLib)
-                           list(APPEND _CCSR_NEW_REQ_LIBS "${_currentLinkInterfaceLib}" )
-                        endif()
-                     endforeach()
-                  endif()
-               endif()
-            else()
-               get_target_property(_importedLocation "${_CURRENT_LIB}" IMPORTED_IMPLIB)
-               if(NOT _importedLocation)
-                  get_target_property(_importedLocation "${_CURRENT_LIB}" IMPORTED_LOCATION)
-               endif()
-               list(APPEND _CCSR_NEW_REQ_LIBS  "${_importedLocation}")
-#               message(STATUS "Appending lib ${_CURRENT_LIB} as ${_importedLocation}")
-
-               get_target_property(_linkInterfaceLibs "${_CURRENT_LIB}" IMPORTED_LINK_INTERFACE_LIBRARIES)
-               if(NOT _linkInterfaceLibs)
-                  get_target_property(_linkInterfaceLibs "${_CURRENT_LIB}" INTERFACE_LINK_LIBRARIES)
-               endif()
-               if(_linkInterfaceLibs)
-                  foreach(_currentLinkInterfaceLib ${_linkInterfaceLibs})
-                     if("${_currentLinkInterfaceLib}" MATCHES "\\\$<LINK_ONLY:(.*)>")
-                        set(_currentLinkInterfaceLib ${CMAKE_MATCH_1})
-                     endif()
-#                     message(STATUS "Appending link interface lib ${_currentLinkInterfaceLib}")
-                     if(_currentLinkInterfaceLib)
-                        list(APPEND _CCSR_NEW_REQ_LIBS "${_currentLinkInterfaceLib}" )
-                     endif()
-                  endforeach()
-               endif()
-            endif()
-         else()
-            # "Normal" libraries are just used as they are.
-            list(APPEND _CCSR_NEW_REQ_LIBS "${_CURRENT_LIB}" )
-#            message(STATUS "Appending lib directly: ${_CURRENT_LIB}")
-         endif()
-      endforeach()
-
-      set(_CCSR_REQ_LIBS ${_CCSR_NEW_REQ_LIBS} )
-   endwhile()
-
-   # Finally we iterate once more over all libraries. This loop only removes
-   # all remaining imported target names (there shouldn't be any left anyway).
-   set(_CCSR_NEW_REQ_LIBS )
-   foreach(_CURRENT_LIB ${_CCSR_REQ_LIBS})
-      if(TARGET "${_CURRENT_LIB}")
-        get_target_property(_importedConfigs "${_CURRENT_LIB}" IMPORTED_CONFIGURATIONS)
-      else()
-        set(_importedConfigs "")
-      endif()
-      if (NOT _importedConfigs)
-         list(APPEND _CCSR_NEW_REQ_LIBS "${_CURRENT_LIB}" )
-#         message(STATUS "final: appending ${_CURRENT_LIB}")
-      else ()
-#             message(STATUS "final: skipping ${_CURRENT_LIB}")
-      endif ()
-   endforeach()
-   if(_CCSR_NEW_REQ_LIBS)
-      list(REMOVE_DUPLICATES _CCSR_NEW_REQ_LIBS)
-   endif()
-#   message(STATUS "setting -${_RESULT}- to -${_CCSR_NEW_REQ_LIBS}-")
-   set(${_RESULT} "${_CCSR_NEW_REQ_LIBS}" PARENT_SCOPE)
-
-endfunction()
-# --- end CMakeExpandImportedTargets.cmake ---
-
-if (BUILD_SHARED_LIBS)
-    set(OpenCV_STATIC OFF)
-else()
-    set(OpenCV_STATIC ON)
-endif()
-include(${CMAKE_CURRENT_LIST_DIR}/../install/OpenCVConfig.cmake)
-cmake_expand_imported_targets(OpenCV_LIBS_DEBUG_EXPANDED LIBRARIES ${OpenCV_LIBS} CONFIGURATION DEBUG)
-cmake_expand_imported_targets(OpenCV_LIBS_RELEASE_EXPANDED LIBRARIES ${OpenCV_LIBS} CONFIGURATION RELEASE)
-set(OpenCV_LIBS ${OpenCV_LIBS_DEBUG_EXPANDED} ${OpenCV_LIBS_RELEASE_EXPANDED})
-message(STATUS __BEGIN__)
-get_cmake_property(_variableNames VARIABLES)
-foreach (_variableName ${_variableNames})
-    string(REGEX MATCH "^OpenCV" _ovar ${_variableName})
-    if(NOT _ovar STREQUAL "")
-        message(STATUS "${_variableName}=${${_variableName}}")
-    endif()
-endforeach()
-message(STATUS __END__)
-"""
-
-
-def _parse_cmake_vars(output):
-    res = {}
-    start_reached = False
-    for line in output.splitlines():
-        if not start_reached:
-            if "__BEGIN__" in line:
-                start_reached = True
-            continue
-        elif "__END__" in line:
-            break
-        mobj = re.match(r"-- (\w+)=(.*)", line)
-        if mobj:
-            name = mobj.group(1)
-            value = mobj.group(2)
-            if ";" in value:
-                value = value.split(";")
-            res[name] = value
-    return res
+TBB_PKG = "TBB/4.4.4@conan/stable"
 
 
 class OpenCVConan(ConanFile):
@@ -303,6 +69,7 @@ class OpenCVConan(ConanFile):
     license = "http://http://opencv.org/license.html"
     generators = "cmake"
     short_paths = True
+    requires = "cmake_config_tools/0.0.1@dbely/testing"
 
     def requirements(self):
         if self.options.with_qt:
@@ -348,42 +115,20 @@ class OpenCVConan(ConanFile):
         self.copy(pattern="*.dll", dst="bin", src="install", keep_path=False)
         self.copy(pattern="*.exe", dst="bin", src="install", keep_path=False)
 
-        detect_dir = os.path.join(self.build_folder, "_detect_opencv")
         opencv_install_dir = os.path.join(self.build_folder, "install")
-        cmakelists_txt_path = os.path.join(detect_dir, "CMakeLists.txt")
-        tools.save(cmakelists_txt_path, CMAKELISTS_TXT)
-
-        cmake = CMake(self)
-        cmd = "cmake . " + cmake.command_line
-        output = StringIO()
-        try:
-            self.run(cmd, cwd=detect_dir, output=output)
-        except:
-            print(output.getvalue())
-            raise
-        cmake_vars = _parse_cmake_vars(output.getvalue())
-        #for (key, value) in cmake_vars.items():
-        #    print("{0}={1}".format(key, value))
-        if cmake_vars["OpenCV_FOUND"] != "TRUE":
-            print(output.getvalue())
-            raise Exception("Failed to find OpenCV")
-        output.close()
-
-        cpp_info = {}
-        cpp_info["includedirs"] = [os.path.relpath(path, opencv_install_dir)
-                                   for path in cmake_vars["OpenCV_INCLUDE_DIRS"]]
-        cpp_info["libs"] = [os.path.basename(lib)
-                            for lib in cmake_vars["OpenCV_LIBS"]]
-        libdirs = []
-        for libpath in cmake_vars["OpenCV_LIBS"]:
-            try:
-                libdirs.append(os.path.relpath(os.path.dirname(libpath), opencv_install_dir))
-            except:
-                # system libs like ws2_32 do not have path component
-                pass
-        cpp_info["libdirs"] = list(set(libdirs))
-        cpp_info["bindirs"] = [os.path.join(os.path.dirname(libdirs[0]), "bin")]
-
+        with tools.pythonpath(self):
+            import cmake_config_tools  # pylint: disable=F0401
+            if self.settings.os == "Windows":
+                cpp_info = cmake_config_tools.cmake_find_package(
+                    self, opencv_install_dir, "OpenCV")
+            else:
+                cpp_info = cmake_config_tools.cmake_find_package(
+                    self, opencv_install_dir, "OpenCV", "share/OpenCV")
+            # work around OpenCV problem (exclude external libs like tbb)
+            cpp_info["libs"] = [lib for lib in cpp_info["libs"]
+                                if not lib in OPENCV_3RDPARTY_PKG]
+            cpp_info["bindirs"] = [os.path.join(
+                os.path.dirname(cpp_info["libdirs"][0]), "bin")]
         cpp_info_json = os.path.join(self.package_folder, "cpp_info.json")
         tools.save(cpp_info_json, json.dumps(cpp_info))
 
